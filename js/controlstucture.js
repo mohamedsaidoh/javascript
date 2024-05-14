9de598947434c5b65f615d59bc61f527dc460a89
mohamedsaidoh <Saidomohamed39@gmail.com>
2024-05-14 15:14:14 +0300

    files

---
+++ js/controlstucture.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+{
+    let greeting;
+    let hour = 27;
+    if ( hour < 3 ){
+     greeting = "Good Night";
+    }else if (hour < 12){
+     greeting = "Good morning";
+    }else if(hour < 16){
+     greeting = "Good afternoon";
+    }else{
+     greeting = "Good evening";
+    }
+    console.log(greeting);
+ }  
+
+
+{
+//A = 85 TO 100
+   // B 70 TO 84
+   // C 60 TO 69
+   // D 50 TO 59
+   //E BELOW 50    
+}
+
+    {
+        let grade =55
+        if (grade >= 85){
+            console.log("A")
+        }
+        else if(grade >=70){
+            console.log("B")
+        }
+        else if(grade >=69){
+            console.log("C")
+        }
+        else if(grade >=50){
+            console.log("D")
+        }
+        else{
+            console.log("F")
+        }
+    }{
+        let leapyear;
+        if(year=2000){
+            console.log("2000")
+        }
+        else if(leapyear=2002){
+            console.log("2002")
+        }
+        else if (leapyear=2004){
+            console.log("2004")
+        }
+
+    }
+
+{
+    let x = 20;
+
+    if(x%7 == 0){
+        console.log("X is divisible by 7")
+    }else{
+        console.log("x is not divisible by 7")
+    }
+
+    //0 - 3 child  should be at home
+    // 3 - 6 should be kindergarten
+    //6-10 lower primary
+    //10 - 14 upper primary
+    //15 - 18 high school
+    //18 - 24 campus
+    //24 - 60 - employed
+    // 60 120 retired
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
---
+++ js/dataTypes.js
@@ -0,0 +1,17 @@
+{
+    console.log(3>10)
+}
+{
+    console.log(3<10)
+}
+{
+    console.log("5"===5)
+}
+{
+    let x=6
+    if (x<10)(
+        console.log("x is less than 10")
+    )else(
+        
+    )
+}
